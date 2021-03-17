dfc601c702cb500b89a14e0d5714e398c24b010d
ndrivdal <ndrivdal@gmail.com>
2021-03-17 17:12:45 +0100

    Added Wolt-code-copy mechanism

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
     // Her skal animasjoner til boksene havne
+
+    $("#WoltBtn").click(function () {
+        $("#WoltKode").css("display", "block")
+
+        document.getElementById("WoltKode").select()
+        document.getElementById("WoltKode").setSelectionRange(0, 99999);
+
+        document.execCommand("copy");
+        $("#WoltKode").css("display", "none")
+    });
+
 });
